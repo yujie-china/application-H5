5d04a95974cd23fa3e4c2a05414002660549dbdb
yujie-china <112388114+yujie-china@users.noreply.github.com>
2023-08-11 15:38:00 +0800

    样式调整

--- src/pages/form/form.vue
+++ src/pages/form/form.vue
@@ -1,11 +1,4 @@
-<!--
- * @Author: Cora huangyinlin@heyme.cn
- * @Date: 2023-08-03 10:54:38
- * @LastEditors: Cora huangyinlin@heyme.cn
- * @LastEditTime: 2023-08-04 10:37:29
- * @FilePath: \heyme.application.h5\src\pages\form\form.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
+
 <template >
     <div class="nav-bar">
         <div class="back" @click="onClickLeft">返回</div>
